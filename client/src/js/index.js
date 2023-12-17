630e9aab13b48a108142fe32b874196fc3488c43
WebDevMullins <webdevmullins@gmail.com>
2023-12-17 11:10:54 -0600

    Fix import statements and implement putDb and getDb functions

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,41 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+import { Workbox } from 'workbox-window'
+import '../css/style.css'
+import './database'
+import Editor from './editor'
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+const main = document.querySelector('#main')
+main.innerHTML = ''
 
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
-  spinner.innerHTML = `
+	const spinner = document.createElement('div')
+	spinner.classList.add('spinner')
+	spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
   </div>
-  `;
-  main.appendChild(spinner);
-};
+  `
+	main.appendChild(spinner)
+}
 
-const editor = new Editor();
+const editor = new Editor()
 
 if (typeof editor === 'undefined') {
-  loadSpinner();
+	loadSpinner()
 }
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
+	// register workbox service worker
+	const workboxSW = new Workbox('/src-sw.js')
+	workboxSW.register()
 } else {
-  console.error('Service workers are not supported in this browser.');
+	console.error('Service workers are not supported in this browser.')
+}
+
+if (module.hot) {
+	module.hot.accept((err) => {
+		if (err) {
+			console.error('Cannot apply HMR update.', err)
+		}
+	})
 }
